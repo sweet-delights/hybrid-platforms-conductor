22beed27467a925d3fb6c0ef1ec9c604318b00aa
Muriel Salvan <muriel@x-aeon.com>
2019-08-09 12:05:46 +0200

    [Feature] More intelligent linear strategy test that handles linear strategies with explicit merge commits

--- lib/hybrid_platforms_conductor/tests/plugins/linear_strategy.rb
+++ lib/hybrid_platforms_conductor/tests/plugins/linear_strategy.rb
@@ -8,13 +8,15 @@ module HybridPlatformsConductor
       class LinearStrategy < Tests::Test
 
         # Number of seconds of the period after which we tolerate non-linear history in git
-        LOOKING_PERIOD = 6 * 31 * 24 * 60 * 60 # 6 months
+        LOOKING_PERIOD = 6 * 31 * 24 * 60 * 60# 6 months
 
         # Check my_test_plugin.rb.sample documentation for signature details.
         def test_on_platform
-          Dir.chdir(@platform.repository_path) do
-            last_log_date_str = `git log --min-parents=2 --pretty=format:%aI`.split("\n").first.strip
-            error 'Git history is not linear' if !last_log_date_str.nil? && Time.now - Time.parse(last_log_date_str) < LOOKING_PERIOD
+          `cd #{@platform.repository_path} && git log --merges --pretty=format:"%H"`.split("\n").each do |merge_commit_id|
+            if !`cd #{@platform.repository_path} && git log $(git merge-base --octopus $(git log #{merge_commit_id} --max-count 1 --pretty=format:"%P"))..#{merge_commit_id} --pretty=format:"%H" --graph | grep '|'`.empty? &&
+              Time.now - Time.parse(`cd #{@platform.repository_path} && git log #{merge_commit_id} --pretty=format:%aI`.strip) < LOOKING_PERIOD
+              error "Git history is not linear because of Merge commit #{merge_commit_id}"
+            end
           end
         end
 
