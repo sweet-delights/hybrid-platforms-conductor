83f4e8b7bd43e0ea665d7e389d8b274cad7647f4
Muriel Salvan <muriel@x-aeon.com>
2020-04-17 15:35:09 +0200

    Corrected test cases

--- spec/hybrid_platforms_conductor_test/executables/report_spec.rb
+++ spec/hybrid_platforms_conductor_test/executables/report_spec.rb
@@ -1,52 +1,55 @@
 describe 'report executable' do
 
   it 'reports by default on all nodes' do
-    with_test_platform(nodes: { 'node1' => {}, 'node2' => {} }) do
+    with_test_platform(nodes: { 'node1' => { service: 'node1_service' }, 'node2' => { service: 'node2_service' } }) do
       exit_code, stdout, stderr = run 'report'
       expect(exit_code).to eq 0
-      expect(stdout).to eq(
-        "+-----------+----------+-------------+------------+----------------+----+---------+----------+---------+-------------+----------------------------+\n" +
-        "| Node name | Platform | Private IPs | Public IPs | Physical node? | OS | Cluster | IP range | Product | Description | Missing industrialization? |\n" +
-        "+-----------+----------+-------------+------------+----------------+----+---------+----------+---------+-------------+----------------------------+\n" +
-        "| node1     | platform |             |            | No             |    |         |          |         |             | No                         |\n" +
-        "| node2     | platform |             |            | No             |    |         |          |         |             | No                         |\n" +
-        "+-----------+----------+-------------+------------+----------------+----+---------+----------+---------+-------------+----------------------------+\n"
-      )
+      expect(stdout).to eq <<~EOS
+        +-----------+----------+-------------+------------+----------------+----------+---------------+-------------+----------------------------+
+        | Node name | Platform | Private IPs | Public IPs | Physical node? | Image ID | Service       | Description | Missing industrialization? |
+        +-----------+----------+-------------+------------+----------------+----------+---------------+-------------+----------------------------+
+        | node1     | platform |             |            | No             |          | node1_service |             | No                         |
+        | node2     | platform |             |            | No             |          | node2_service |             | No                         |
+        +-----------+----------+-------------+------------+----------------+----------+---------------+-------------+----------------------------+
+      EOS
       expect(stderr).to eq ''
     end
   end
 
   it 'reports on given nodes only' do
-    with_test_platform(nodes: { 'node1' => {}, 'node2' => {} }) do
+    with_test_platform(nodes: { 'node1' => { service: 'node1_service' }, 'node2' => { service: 'node2_service' } }) do
       exit_code, stdout, stderr = run 'report', '--node', 'node2'
       expect(exit_code).to eq 0
-      expect(stdout).to eq(
-        "+-----------+----------+-------------+------------+----------------+----+---------+----------+---------+-------------+----------------------------+\n" +
-        "| Node name | Platform | Private IPs | Public IPs | Physical node? | OS | Cluster | IP range | Product | Description | Missing industrialization? |\n" +
-        "+-----------+----------+-------------+------------+----------------+----+---------+----------+---------+-------------+----------------------------+\n" +
-        "| node2     | platform |             |            | No             |    |         |          |         |             | No                         |\n" +
-        "+-----------+----------+-------------+------------+----------------+----+---------+----------+---------+-------------+----------------------------+\n"
-      )
+      expect(stdout).to eq <<~EOS
+        +-----------+----------+-------------+------------+----------------+----------+---------------+-------------+----------------------------+
+        | Node name | Platform | Private IPs | Public IPs | Physical node? | Image ID | Service       | Description | Missing industrialization? |
+        +-----------+----------+-------------+------------+----------------+----------+---------------+-------------+----------------------------+
+        | node2     | platform |             |            | No             |          | node2_service |             | No                         |
+        +-----------+----------+-------------+------------+----------------+----------+---------------+-------------+----------------------------+
+      EOS
       expect(stderr).to eq ''
     end
   end
 
   it 'reports info from metadata' do
-    with_test_platform(nodes: { 'node' => { meta: {
-      'private_ips' => ['192.168.0.1', '192.168.0.2'],
-      'public_ips' => ['1.2.3.4'],
-      'os' => 'Windows 3.1',
-      'description' => 'A great server'
-    } } }) do
+    with_test_platform(nodes: { 'node' => {
+       meta: {
+        'private_ips' => ['192.168.0.1', '192.168.0.2'],
+        'public_ips' => ['1.2.3.4'],
+        'image' => 'debian_10',
+        'description' => 'A great server'
+      },
+      service: 'node_service'
+    } }) do
       exit_code, stdout, stderr = run 'report', '--node', 'node'
       expect(exit_code).to eq 0
-      expect(stdout).to eq(
-        "+-----------+----------+-------------------------+------------+----------------+-------------+---------+-------------+---------+----------------+----------------------------+\n" +
-        "| Node name | Platform | Private IPs             | Public IPs | Physical node? | OS          | Cluster | IP range    | Product | Description    | Missing industrialization? |\n" +
-        "+-----------+----------+-------------------------+------------+----------------+-------------+---------+-------------+---------+----------------+----------------------------+\n" +
-        "| node      | platform | 192.168.0.1 192.168.0.2 | 1.2.3.4    | No             | Windows 3.1 |         | 192.168.0.* |         | A great server | No                         |\n" +
-        "+-----------+----------+-------------------------+------------+----------------+-------------+---------+-------------+---------+----------------+----------------------------+\n"
-      )
+      expect(stdout).to eq <<~EOS
+        +-----------+----------+-------------------------+------------+----------------+-----------+--------------+----------------+----------------------------+
+        | Node name | Platform | Private IPs             | Public IPs | Physical node? | Image ID  | Service      | Description    | Missing industrialization? |
+        +-----------+----------+-------------------------+------------+----------------+-----------+--------------+----------------+----------------------------+
+        | node      | platform | 192.168.0.1 192.168.0.2 | 1.2.3.4    | No             | debian_10 | node_service | A great server | No                         |
+        +-----------+----------+-------------------------+------------+----------------+-----------+--------------+----------------+----------------------------+
+      EOS
       expect(stderr).to eq ''
     end
   end
