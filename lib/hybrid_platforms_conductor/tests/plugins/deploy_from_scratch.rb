6aa618c757e443addb521efde838008cbc0f30c9
Muriel Salvan <muriel@x-aeon.com>
2020-07-31 15:50:15 +0200

    Removed useless debug log

--- lib/hybrid_platforms_conductor/tests/plugins/deploy_from_scratch.rb
+++ lib/hybrid_platforms_conductor/tests/plugins/deploy_from_scratch.rb
@@ -13,7 +13,6 @@ module HybridPlatformsConductor
         def test_for_node
           @deployer.with_docker_container_for(@node, container_id: 'deploy_from_scratch', reuse_container: log_debug?) do |deployer|
             deployer.nbr_retries_on_error = 3
-            deployer.log_level = :debug
             result = deployer.deploy_on(@node)
             assert_equal result.size, 1, "Wrong number of nodes being tested: #{result.size}"
             tested_node, (exit_status, _stdout, _stderr) = result.first
