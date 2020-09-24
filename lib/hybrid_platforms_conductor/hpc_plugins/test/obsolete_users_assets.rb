1f86912cbad662498a14c68c3416c8a9f211ab95
Muriel Salvan <muriel@x-aeon.com>
2020-09-24 17:41:13 +0200

    [Feature] Add test plugin to check for obsolete users assets still present on the nodes

---
+++ lib/hybrid_platforms_conductor/hpc_plugins/test/obsolete_users_assets.rb
@@ -0,0 +1,52 @@
+require 'hybrid_platforms_conductor-chef/test'
+
+module HybridPlatformsConductor
+
+  module HpcPlugins
+
+    module Test
+
+      # Test that the node has no home directories belonging to obsolete users
+      class ObsoleteUsersAssets < HybridPlatformsConductor::Test
+
+        TESTED_HOME_POINTS = [
+          '/home',
+          '/mnt/users'
+        ]
+
+        # Check my_test_plugin.rb.sample documentation for signature details.
+        def test_on_node
+          # Read the directory
+          obsolete_home_dirs =
+            (@platform.metadata.dig('test', 'obsolete_users') || []).map do |user_name|
+              TESTED_HOME_POINTS.map do |home_base_dir|
+                "#{home_base_dir}/#{user_name}"
+              end
+            end.flatten
+          Hash[obsolete_home_dirs.map do |home_dir|
+            [
+              "if sudo /bin/bash -c '[[ -d \"#{home_dir}\" ]]' ; then echo 1 ; else echo 0 ; fi",
+              {
+                validator: proc do |stdout|
+                  case stdout
+                  when ['1']
+                    error "Obsolete home dir found: #{home_dir}"
+                  when ['0']
+                    # Perfect :D
+                  else
+                    error "Could not check for existence of #{home_dir}: #{stdout.join("\n")}"
+                  end
+                end,
+                timeout: 2
+              }
+            ]
+          end]
+        end
+
+      end
+
+    end
+
+  end
+
+end
