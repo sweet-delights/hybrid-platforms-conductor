7f700532d2988ba89b3feb5139695cb1212c6207
Muriel Salvan <muriel@x-aeon.com>
2020-09-24 17:43:39 +0200

    Better Ruby

--- lib/hybrid_platforms_conductor/hpc_plugins/test/obsolete_users_assets.rb
+++ lib/hybrid_platforms_conductor/hpc_plugins/test/obsolete_users_assets.rb
@@ -16,31 +16,30 @@ module HybridPlatformsConductor
 
         # Check my_test_plugin.rb.sample documentation for signature details.
         def test_on_node
-          # Read the directory
-          obsolete_home_dirs =
+          Hash[
             (@platform.metadata.dig('test', 'obsolete_users') || []).map do |user_name|
               TESTED_HOME_POINTS.map do |home_base_dir|
                 "#{home_base_dir}/#{user_name}"
               end
-            end.flatten
-          Hash[obsolete_home_dirs.map do |home_dir|
-            [
-              "if sudo /bin/bash -c '[[ -d \"#{home_dir}\" ]]' ; then echo 1 ; else echo 0 ; fi",
-              {
-                validator: proc do |stdout|
-                  case stdout
-                  when ['1']
-                    error "Obsolete home dir found: #{home_dir}"
-                  when ['0']
-                    # Perfect :D
-                  else
-                    error "Could not check for existence of #{home_dir}: #{stdout.join("\n")}"
-                  end
-                end,
-                timeout: 2
-              }
-            ]
-          end]
+            end.flatten.map do |home_dir|
+              [
+                "if sudo /bin/bash -c '[[ -d \"#{home_dir}\" ]]' ; then echo 1 ; else echo 0 ; fi",
+                {
+                  validator: proc do |stdout|
+                    case stdout
+                    when ['1']
+                      error "Obsolete home dir found: #{home_dir}"
+                    when ['0']
+                      # Perfect :D
+                    else
+                      error "Could not check for existence of #{home_dir}: #{stdout.join("\n")}"
+                    end
+                  end,
+                  timeout: 2
+                }
+              ]
+            end
+          ]
         end
 
       end
