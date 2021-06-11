51d1c0afb5fcb73762568802883652b9786807ce
Muriel Salvan <muriel@x-aeon.com>
2021-06-11 20:05:22 +0200

    Corrected lint Style/NestedParenthesizedCalls

--- spec/hybrid_platforms_conductor_test/api/cmd_runner_spec.rb
+++ spec/hybrid_platforms_conductor_test/api/cmd_runner_spec.rb
@@ -9,14 +9,14 @@ describe HybridPlatformsConductor::CmdRunner do
 
   it 'runs a simple bash command and returns exit code, stdout and stderr correctly' do
     with_repository do
-      expect(test_cmd_runner.run_cmd "echo TestStderr 1>&2 ; echo TestStdout").to eq [0, "TestStdout\n", "TestStderr\n"]
+      expect(test_cmd_runner.run_cmd("echo TestStderr 1>&2 ; echo TestStdout")).to eq [0, "TestStdout\n", "TestStderr\n"]
     end
   end
 
   it 'runs a simple bash command and forces usage of bash' do
     with_repository do
       # Use set -o pipefail that does not work in /bin/sh
-      expect(test_cmd_runner.run_cmd "set -o pipefail ; echo TestStderr 1>&2 ; echo TestStdout", force_bash: true).to eq [0, "TestStdout\n", "TestStderr\n"]
+      expect(test_cmd_runner.run_cmd("set -o pipefail ; echo TestStderr 1>&2 ; echo TestStdout", force_bash: true)).to eq [0, "TestStdout\n", "TestStderr\n"]
     end
   end
 
@@ -65,19 +65,19 @@ describe HybridPlatformsConductor::CmdRunner do
 
   it 'does not fail when the command exits with the expected code' do
     with_repository do
-      expect(test_cmd_runner.run_cmd 'exit 2', expected_code: 2).to eq [2, '', '']
+      expect(test_cmd_runner.run_cmd('exit 2', expected_code: 2)).to eq [2, '', '']
     end
   end
 
   it 'does not fail when the command exits with one of the expected codes' do
     with_repository do
-      expect(test_cmd_runner.run_cmd 'exit 2', expected_code: [0, 2, 3]).to eq [2, '', '']
+      expect(test_cmd_runner.run_cmd('exit 2', expected_code: [0, 2, 3])).to eq [2, '', '']
     end
   end
 
   it 'does not fail when the command does not exit 0 and we specify no exception' do
     with_repository do
-      expect(test_cmd_runner.run_cmd 'exit 1', no_exception: true).to eq [1, '', '']
+      expect(test_cmd_runner.run_cmd('exit 1', no_exception: true)).to eq [1, '', '']
     end
   end
 
@@ -107,25 +107,25 @@ describe HybridPlatformsConductor::CmdRunner do
 
   it 'returns the timeout error when the command times out and we specify no exception' do
     with_repository do
-      expect(test_cmd_runner.run_cmd 'sleep 5', timeout: 1, no_exception: true).to eq [:timeout, '', 'Timeout of 1 triggered']
+      expect(test_cmd_runner.run_cmd('sleep 5', timeout: 1, no_exception: true)).to eq [:timeout, '', 'Timeout of 1 triggered']
     end
   end
 
   it 'returns the timeout error when the command is expected to time out' do
     with_repository do
-      expect(test_cmd_runner.run_cmd 'sleep 5', timeout: 1, expected_code: :timeout).to eq [:timeout, '', 'Timeout of 1 triggered']
+      expect(test_cmd_runner.run_cmd('sleep 5', timeout: 1, expected_code: :timeout)).to eq [:timeout, '', 'Timeout of 1 triggered']
     end
   end
 
   it 'returns the timeout error with previously output stdout and stderr when the command times out and we specify no exception' do
     with_repository do
-      expect(test_cmd_runner.run_cmd 'echo TestStderr 1>&2 ; sleep 1 ; echo TestStdout ; sleep 5 ; echo NeverDisplayed', timeout: 2, no_exception: true).to eq [:timeout, "TestStdout\n", "TestStderr\n\nTimeout of 2 triggered"]
+      expect(test_cmd_runner.run_cmd('echo TestStderr 1>&2 ; sleep 1 ; echo TestStdout ; sleep 5 ; echo NeverDisplayed', timeout: 2, no_exception: true)).to eq [:timeout, "TestStdout\n", "TestStderr\n\nTimeout of 2 triggered"]
     end
   end
 
   it 'returns the timeout error with previously output stdout and stderr when the command times out as expected' do
     with_repository do
-      expect(test_cmd_runner.run_cmd 'echo TestStderr 1>&2 ; sleep 1 ; echo TestStdout ; sleep 5 ; echo NeverDisplayed', timeout: 2, expected_code: :timeout).to eq [:timeout, "TestStdout\n", "TestStderr\n\nTimeout of 2 triggered"]
+      expect(test_cmd_runner.run_cmd('echo TestStderr 1>&2 ; sleep 1 ; echo TestStdout ; sleep 5 ; echo NeverDisplayed', timeout: 2, expected_code: :timeout)).to eq [:timeout, "TestStdout\n", "TestStderr\n\nTimeout of 2 triggered"]
     end
   end
 
@@ -133,7 +133,7 @@ describe HybridPlatformsConductor::CmdRunner do
     with_repository do |repository|
       cmd_runner = test_cmd_runner
       cmd_runner.dry_run = true
-      expect(cmd_runner.run_cmd "echo TestContent >#{repository}/test_file").to eq [0, '', '']
+      expect(cmd_runner.run_cmd("echo TestContent >#{repository}/test_file")).to eq [0, '', '']
       expect(File.exist?("#{repository}/test_file")).to eq false
     end
   end
@@ -142,7 +142,7 @@ describe HybridPlatformsConductor::CmdRunner do
     with_repository do |repository|
       cmd_runner = test_cmd_runner
       cmd_runner.dry_run = true
-      expect(cmd_runner.run_cmd "echo TestContent >#{repository}/test_file", expected_code: 2).to eq [2, '', '']
+      expect(cmd_runner.run_cmd("echo TestContent >#{repository}/test_file", expected_code: 2)).to eq [2, '', '']
       expect(File.exist?("#{repository}/test_file")).to eq false
     end
   end
