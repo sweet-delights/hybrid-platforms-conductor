f2e5bcc59b595592bf0a2b4f63c92966f10902ef
Muriel Salvan <muriel@x-aeon.com>
2021-08-05 18:50:41 +0200

    [Hotfix] Added test checking that test keys are not deployed in production for security

--- spec/hybrid_platforms_conductor_test/api/platform_handlers/serverless_chef/packaging_spec.rb
+++ spec/hybrid_platforms_conductor_test/api/platform_handlers/serverless_chef/packaging_spec.rb
@@ -330,7 +330,7 @@ describe HybridPlatformsConductor::HpcPlugins::PlatformHandler::ServerlessChef d
         end
       end
 
-      it 'packages the repository with a testadmin public key' do
+      it 'packages the repository with a testadmin public key in local mode' do
         with_serverless_chef_platforms('hpc_test') do |platform, repository|
           File.write("#{ENV['hpc_platforms']}/testadmin.key.pub", 'ssh-rsa 12345 testadmin@test.com')
           with_packaging_mocked(
@@ -345,6 +345,21 @@ describe HybridPlatformsConductor::HpcPlugins::PlatformHandler::ServerlessChef d
             testadmin_key_pub = Dir.glob("#{repository}/dist/local/test_policy/cookbook_artifacts/hpc_test-*/files/default/testadmin.key.pub").first
             expect(testadmin_key_pub).not_to eq nil
             expect(File.read(testadmin_key_pub)).to eq 'ssh-rsa 12345 testadmin@test.com'
+          end
+        end
+      end
+
+      it 'does not package the repository with a testadmin public key in prod mode' do
+        with_serverless_chef_platforms('hpc_test') do |platform, repository|
+          File.write("#{ENV['hpc_platforms']}/testadmin.key.pub", 'ssh-rsa 12345 testadmin@test.com')
+          with_packaging_mocked(
+            repository,
+            cookbook_metadata: {
+              'hpc_test-1234' => {}
+            }
+          ) do
+            platform.package(services: { 'node' => %w[test_policy] }, secrets: {}, local_environment: false)
+            expect(Dir.glob("#{repository}/dist/prod/test_policy/cookbook_artifacts/hpc_test-*/files/default/testadmin.key.pub")).to eq []
           end
         end
       end
