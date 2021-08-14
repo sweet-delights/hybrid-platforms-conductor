08ae9cd665205ab752b918319859cc587b2cd0f9
Muriel Salvan <muriel@x-aeon.com>
2021-08-14 19:19:49 +0200

    [Fix(test_github_ci)] [#95] Make sure the github_ci test does not fail when a repository has no workflow run

--- lib/hybrid_platforms_conductor/hpc_plugins/test/github_ci.rb
+++ lib/hybrid_platforms_conductor/hpc_plugins/test/github_ci.rb
@@ -18,10 +18,15 @@ module HybridPlatformsConductor
         def test
           for_each_github_repo do |client, repo_info|
             log_debug "Checking CI for Github repository #{repo_info[:slug]}"
-            last_status = client.repository_workflow_runs(repo_info[:slug])[:workflow_runs].
+            last_run = client.repository_workflow_runs(repo_info[:slug])[:workflow_runs].
               select { |run| run[:head_branch] == 'master' }.
-              max_by { |run| run[:created_at] }[:conclusion]
-            error "Last workflow status for repository #{repo_info[:slug]} is #{last_status}" unless last_status == 'success'
+              max_by { |run| run[:created_at] }
+            if last_run.nil?
+              error "No workflow run found for repository #{repo_info[:slug]}"
+            else
+              last_status = last_run[:conclusion]
+              error "Last workflow status for repository #{repo_info[:slug]} is #{last_status}" unless last_status == 'success'
+            end
           end
         end
 
