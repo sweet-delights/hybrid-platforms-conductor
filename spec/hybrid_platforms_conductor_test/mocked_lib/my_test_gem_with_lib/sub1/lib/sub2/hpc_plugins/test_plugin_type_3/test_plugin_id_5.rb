cde89cd222f11ce1a7b0b55f20509cd90f586bf6
Muriel Salvan <muriel@x-aeon.com>
2022-03-20 14:09:47 +0100

    Added test plugin with lib in path

---
+++ spec/hybrid_platforms_conductor_test/mocked_lib/my_test_gem_with_lib/sub1/lib/sub2/hpc_plugins/test_plugin_type_3/test_plugin_id_5.rb
@@ -0,0 +1,34 @@
+module HybridPlatformsConductorTest
+
+  module MockedLib
+
+    module MyTestGemWithLib
+
+      module Sub1
+
+        module Lib
+
+          module Sub2
+
+            module HpcPlugins
+
+              module TestPluginType3
+
+                class TestPluginId5 < HybridPlatformsConductor::TestPluginType3
+                end
+
+              end
+
+            end
+
+          end
+
+        end
+
+      end
+
+    end
+
+  end
+
+end
